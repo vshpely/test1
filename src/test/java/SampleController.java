a8c35290b56d449b722417be02dccf8e8d101ba1
Шпелик Віталій <vshpelyk@berta.corp>
2016-12-08 13:51:06 +0200

    Initial commit

---
+++ src/test/java/SampleController.java
@@ -0,0 +1,22 @@
+/**
+ * Created by vshpelyk on 05.12.2016.
+ */
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleController.class, args);
+    }
+}
